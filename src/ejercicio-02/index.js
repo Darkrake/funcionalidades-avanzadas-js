2ef220042b25cb63fe19c3f0607938533dace0be
Darkrake <rachel_1723@hotmail.com>
2023-11-18 06:02:29 +0100

    Añadir ejer.2

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,19 @@
 // Escribe tu código aquí:
+const person = {
+    name: 'John',
+    greet: function () {
+      console.log(`Hola, soy ${this.name}`)
+    }
+  }
+  
+  person.greet() // Hola, soy John
+  
+  // Cambiando a una arrow function
+  const person2 = {
+    name: 'Alice',
+    greet: () => {
+      console.log(`Hola, soy ${this.name}`)
+    }
+  }
+  
+  person2.greet() 
