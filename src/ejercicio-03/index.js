a1ddd6dbfc8a181547843d2e690158bb3e8d3791
Darkrake <rachel_1723@hotmail.com>
2023-11-18 06:08:50 +0100

    Añadir ejer.3

--- src/ejercicio-03/index.js
+++ src/ejercicio-03/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+const person = {
+    name: 'Rachel',
+    age: 33,
+    city: 'Canet de Berenguer'
+  }
+  
+  const message = `Hola, mi nombre es ${person.name}, tengo ${person.age} años y vivo en ${person.city}.`
+  
+  console.log(message) 
